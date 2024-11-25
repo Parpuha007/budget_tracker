835372d5df8f6c9b965ae29ec9d5ee271faf5085
Рустам Парпиев <r.parpiev@ics78.ru>
2024-11-25 14:23:10 +0700

    fix: :bug: убрал комменты

--- lib/app/presentation/app_builder.dart
+++ lib/app/presentation/app_builder.dart
@@ -7,61 +7,7 @@ import 'package:provider/single_child_widget.dart';
 class MainAppBuilder extends AppBuilder {
   MainAppBuilder(this.appRouter);
 
-  List<SingleChildWidget> get providers => [
-        // Provider(create: (_) => getIt<AppThemeData>()),
-        // BlocProvider.value(
-        //   value: getIt<AuthStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<OnBoardingCacheManagerStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<DocumentsLinksStore>()..init(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<FirstLaunchStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<UserStore>()..init(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<RegistrationOnBoardingStore>()
-        //     ..setNavigationState(
-        //       OnBoardingInitialScreenState(),
-        //     )
-        //     ..getCurrentStage(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<HomeStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<LocalNotificationsStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<SplashStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<AppUpdateStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<InternetCheckerStore>()..init(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<AnalyticsChartPositionStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<FundsWithdrawalStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<CheckAddingAccountPossibilityStore>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<OnboardingTourCubit>(),
-        // ),
-        // BlocProvider.value(
-        //   value: getIt<AppReviewBloc>(),
-        // ),
-      ];
+  List<SingleChildWidget> get providers => [];
 
   final RootStackRouter appRouter;
 
@@ -81,18 +27,11 @@ class MainAppBuilder extends AppBuilder {
           appRouter,
           navigatorObservers: () => [
             AutoRouteObserver(),
-            // TalkerRouteObserver(talker),
           ],
         ),
-        // localizationsDelegates: [
-        //   ...context.localizationDelegates,
-        // ],
-        // supportedLocales: context.supportedLocales,
-        // locale: context.locale,
         routeInformationProvider: appRouter.routeInfoProvider(),
         routeInformationParser: appRouter.defaultRouteParser(),
         themeMode: ThemeMode.light,
-        // theme: getIt<AppThemeData>().themeData,
       ),
     );
   }
